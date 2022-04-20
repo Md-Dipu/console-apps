820defe4574548fb4aef27a793b82de67a5b96a6
Md. Dipu <srabun.tangail@gmail.com>
2022-04-20 23:06:05 +0600

    [quary]arguments

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "services.h"
+#include "arguments.h"
 
 int main(const int argc, const char *argv[])
 {
+    // arguments
+    for (int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] == '/' || argv[i][0] == '-')
+        {
+        }
+    }
+
+    // actions
     for (int i = 1; i < argc; i++)
     {
         const char *filename = argv[i];
