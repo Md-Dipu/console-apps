09048502fa8f2761cd115e7ef1f01a9bc294a7ba
Md. Dipu <srabun.tangail@gmail.com>
2022-04-20 23:29:05 +0600

    filtering arg and filename

--- src/main.c
+++ src/main.c
@@ -10,15 +10,22 @@ int main(const int argc, const char *argv[])
     // arguments
     for (int i = 1; i < argc; i++)
     {
-        if (argv[i][0] == '/' || argv[i][0] == '-')
+        if (*(*(argv + i)) == '/' || *(*(argv + i)) == '-')
         {
+            char *arg_str = (char *)malloc(strlen(*(argv + i)) * sizeof(char));
+            strcpy(arg_str, *(argv + i));
+            arg_str++;
+            check_arg(arg_str);
         }
     }
 
     // actions
     for (int i = 1; i < argc; i++)
     {
-        const char *filename = argv[i];
+        if (*(*(argv + i)) == '/' || *(*(argv + i)) == '-')
+            break;
+
+        const char *filename = *(argv + i);
         if (!file_exists(filename))
             file_creator(filename);
         else
